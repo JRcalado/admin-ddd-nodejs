2e5ab763421a06206f4f4e69eb7fe2df96c95862
Calado <jricardoc10@gmail.com>
2022-07-27 18:16:18 -0300

    Implementando  testes

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,5 +1,42 @@
-export class Category{
-    constructor(public name: string){
 
+export type CategoryProperties = {
+     name: string,
+     is_active?: boolean,
+     description?: string,
+     created_at?: Date,
+     
+ }
+
+
+export class Category{
+    constructor(public readonly props: CategoryProperties){
+        this.description = this.props.description;
+        this.is_active = this.props.is_active ?? true;
+        this.props.created_at = this.props.created_at ?? new Date();
+    }
+
+
+    get name(): string {
+        return this.props.name;
+    }
+
+    get description(): string | undefined {
+        return this.props.description;
+    }
+
+    private set description(value){
+        this.props.description = value ?? null;
+    }
+
+    get is_active(): boolean | undefined {
+        return this.props.is_active;
+    }
+
+    private set is_active(value){
+        this.props.is_active = value ?? true;
+    }
+
+    get created_at(): Date | undefined {
+        return this.props.created_at;
     }
 }
